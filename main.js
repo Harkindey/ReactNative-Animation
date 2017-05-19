9a028ea8864d732be5503b42d9109d7c93fed018
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-20 00:54:07 +0100

    Writing Reusable Components

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ class App extends React.Component {
         <Deck
           data={DATA}
           renderCard={this.renderCard}
+          onSwipeRight={() => console.log('something was swipped')}
         />
       </View>
     );
